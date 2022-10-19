9ccb4636e8693b889191eb27a56b4f78210c2e2d
unknown <jcuetos97@gmail.com>
2022-10-19 12:08:20 -0500

    HtmlWebpackPlugin added

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,13 +18,36 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin'
+      }),
+
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'service-worker.js',
+      }),
+
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
       ],
-    },
-  };
-};
+    }
+}
+}
