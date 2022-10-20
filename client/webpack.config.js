bacb88880137ce4c160bb0bfe433c3c3aea572e0
unknown <jcuetos97@gmail.com>
2022-10-20 11:55:58 -0500

    setup package.json, register route added

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,8 +4,6 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -17,14 +15,34 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'Webpack Plugin'
       }),
 
+      new WebpackPwaManifest({
+        name: 'J.A.T.E.',
+        short_name:'JATE',
+        description: 'Text Editor', 
+        display: 'standalone',
+        background_color: '#1e1e1e',
+				theme_color: '#1e1e1e',
+        start_url: '/',
+        publicPath: '/',
+        inject: true,
+        icons: [
+          {
+            src:path.resolve('src/images/logo.png'), 
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'), 
+          }
+        ], 
+      }),
+
       new InjectManifest({
-        swSrc: './src/sw.js',
+        swSrc: './src-sw.js',
         swDest: 'service-worker.js',
       }),
 
@@ -43,7 +61,11 @@ module.exports = () => {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
+            presets: ['@babel/preset-env'],
+            plugins: [
+              '@babel/plugin-proposal-object-rest-spread',
+              '@babel/transform-runtime',
+            ]
           }
         }
       },
