d958ab54b5e5ea2652358bb3798f27ab503d2ef1
unknown <jcuetos97@gmail.com>
2022-10-21 09:40:36 -0500

    webpack install test

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,24 @@
 const butInstall = document.getElementById('buttonInstall');
+const textHeader = document.getElementById('textHeader');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    butInstall.style.visibility = 'visible';
+    textHeader.textContent = 'Click the button to install!';
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+    // Click event handler on the `butInstall` element
+    butInstall.addEventListener('click', () => {
+        event.prompt();
+        butInstall.setAttribute('disabled', true);
+        butInstall.textContent = 'Installed!';
+    });
+
+});
+
+// Handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    textHeader.textContent = 'Successfully installed!';
+    console.log('👍', 'appinstalled', event);
+});
